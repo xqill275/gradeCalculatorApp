929891f996967b04c8839a68323bcde3fc21413f
xqill275 <oliverlong275@gmail.com>
2025-03-23 12:05:06 +0000

    profile pic sort of working

--- app/src/main/java/com/example/gradecalculatorapp/User.java
+++ app/src/main/java/com/example/gradecalculatorapp/User.java
@@ -18,10 +18,14 @@ public class User {
     @ColumnInfo(name = "Password")
     public String password;
 
+    @ColumnInfo(name = "ProfilePicLocation")
+    public String profilePicLocation;
+
     // Constructor
     public User(String email, String username, String password) {
         this.email = email;
         this.username = username;
         this.password = password;
+        this.profilePicLocation = null;
     }
 }
